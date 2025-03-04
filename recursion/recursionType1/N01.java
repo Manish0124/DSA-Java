162083308b801d62333ce80310df32b3e26c3060
Manish <techno8427@gmail.com>
2025-03-04 23:36:01 +0530

    chore : some problem added

---
+++ recursion/recursionType1/N01.java
@@ -0,0 +1,23 @@
+package recursion.recursionType1;
+
+public class N01 {
+    public static void main(String[] args) {
+        fun(5);
+        // funRev(5);
+    }
+ 
+    static void fun(int n){
+        if (n==0) {
+            return;
+        }
+        System.out.println(n);
+        fun(n-1);
+    }
+    static void funRev(int n){
+        if (n==0) {
+            return;
+        }
+        funRev(n-1);
+        System.out.println(n);
+    }
+}
