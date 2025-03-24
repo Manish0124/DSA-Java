151b6411288bdca8be17e6b14f030a14c0ff9cee
Manish <techno8427@gmail.com>
2025-03-24 17:11:59 +0530

    chore permutation added

---
+++ QonRecursion/permutations/dice.java
@@ -0,0 +1,19 @@
+package QonRecursion.permutations;
+
+public class dice {
+    public static void main(String[] args) {
+        Dice("", 4);
+    }
+
+    static void Dice(String p , int target){
+            if (target == 0) {
+                System.out.println(p);
+                return;
+            }
+
+            for (int i = 1; i <= 6 && i<=target; i++) {
+                Dice(p+i , target -i );
+                   
+            }
+    }
+}
